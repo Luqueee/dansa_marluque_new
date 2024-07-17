4563c626ec2ee4863b85dbd6263ef608ea00f580
Adrià Cabrera <adriacabrera2007@gmail.com>
2024-07-17 16:58:39 +0200

    chore: Update Astro configuration to import manifest and seoConfig from '@/src/utils/seoConfig'

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import { VitePWA } from 'vite-plugin-pwa';
 import starlight from '@astrojs/starlight';
-import { manifest, seoConfig } from './src/utils/seoConfig';
+import { manifest, seoConfig } from '@/src/utils/seoConfig';
 // https://astro.build/config
 export default defineConfig({
     site: seoConfig.baseURL,
@@ -55,9 +55,6 @@ export default defineConfig({
     vite: {
         build: {
             cssMinify: 'lightningcss',
-        },
-        ssr: {
-            noExternal: ['path-to-regexp'],
         },
 
         plugins: [
