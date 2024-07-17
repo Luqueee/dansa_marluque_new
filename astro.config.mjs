10a8b9de636baf44b17b4b127ee43c4026e65371
Adrià Cabrera <adriacabrera2007@gmail.com>
2024-07-17 16:56:20 +0200

    chore: Add noExternal option for 'path-to-regexp' in Astro SSR configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,6 +56,9 @@ export default defineConfig({
         build: {
             cssMinify: 'lightningcss',
         },
+        ssr: {
+            noExternal: ['path-to-regexp'],
+        },
 
         plugins: [
             VitePWA({
